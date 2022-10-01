b1030285b2e75efa8ccc94ce224b356247678f2c
JuanP-VT <cv.juanp@gmail.com>
2022-10-01 10:01:17 -0600

    ADD se añadió rutas para las categorias

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
-//Modelos de la base de datos
-const Categoria = require("./models/Categoria");
 //Conexión a la base de datos
 mongoose.connect(
   "mongodb+srv://PB:OlaTech@cluster0.mgq46sx.mongodb.net/?retryWrites=true&w=majority",
@@ -11,24 +9,8 @@ mongoose.connect(
     console.log("Connected to MongoDB");
   }
 );
-//Este endpoint agregará una nueva categoria a la base de datos
-app.post("/addcategoria", async (req, res) => {
-  try {
-    //Primero validamos que el nombre no esté ya en la base de datos
-    const search = await Categoria.where("name").equals(req.body.name);
-    if (search.length !== 0) {
-      res.send({ res: "La categoria ya está en la base de datos" });
-      return;
-    }
-    //Si el nombre esta disponible continuamos creando la nueva categoria
-    // Formateamos a lowercase
-    const categoriaName = req.body.name.toLowerCase();
-    const newCategoria = new Categoria({ name: categoriaName });
-    newCategoria.save();
-    res.send(newCategoria);
-  } catch {
-    res.status(500).send();
-  }
-});
+//Este endpoint agrega y consulta las categorias existentes
+const userRouter = require("./routes/Categorias");
+app.use("/categorias", userRouter);
 
-app.listen(3000);
+app.listen(3100);
---
+++ src/routes/Categorias.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const Categoria = require("../models/Categoria");
+
+//Este endpoint agregará una nueva categoria a la base de datos
+router.post("/", async (req, res) => {
+  try {
+    //Primero validamos que el nombre no esté ya en la base de datos
+    const search = await Categoria.where("name").equals(req.body.name);
+    if (search.length !== 0) {
+      res.send({ res: "La categoria ya está en la base de datos" });
+      return;
+    }
+    //Si el nombre esta disponible continuamos creando la nueva categoria
+    // Formateamos a lowercase
+    const categoriaName = req.body.name.toLowerCase();
+    const newCategoria = new Categoria({ name: categoriaName });
+    newCategoria.save();
+    res.send({ res: "Categoria creada con exito", categoria: newCategoria });
+  } catch (error) {
+    res.status(500).send({ res: error.message });
+  }
+});
+
+module.exports = router;
